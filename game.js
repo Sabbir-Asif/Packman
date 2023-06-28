c7d9d54426d3fa9a16710426edfa3738a99d6888
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2023-06-28 13:37:43 +0600

    developing game

--- game.js
+++ game.js
@@ -104,3 +104,59 @@ let update = () =>
         onGhostCollision();
     }
 };
+
+let drawFoods = () =>
+{
+    for(let i = 0; i < map.length; i++)
+    {
+        for(let j = 0; j < map[0].length; j++)
+        {
+            if(map[i][j] == 2)
+            {
+                createRect(
+                    j * oneBlockSize + oneBlockSize / 3,
+                    i * oneBlockSize + oneBlockSize / 3,
+                    oneBlockSize / 3,
+                    oneBlockSize / 3,
+                    "#FEB897"
+                );
+            }
+        }
+    }
+};
+
+let drawRemainingLives = () =>
+{
+    canvasContext.font = "20px Emulogic";
+    canvasContext.fillStyle = "white";
+    canvasContext.fillText("Lives : ", 220), oneBlockSize * (map.length + 1);
+
+    for(let i = 0; i < lives; i++)
+    {
+        canvasContext.drawImage(
+            pacmanFrames,
+            2 * oneBlockSize,
+            0,
+            oneBlockSize,
+            oneBlockSize,
+            350 + i * oneBlockSize,
+            oneBlockSize * map.length,
+            oneBlockSize * map.length + 2,
+            oneBlockSize,
+            oneBlockSize
+        );
+    }
+};
+
+let drawScore = () =>
+{
+    canvasContext.font = "20px Emulogic";
+    canvasContext.fillStyle = "white";
+    canvasContext.fillText(
+        "Score : " + score,
+        0,
+        oneBlockSize * (map.length + 1)
+    );
+};
+
+
