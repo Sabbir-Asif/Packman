4d82769eb4ca5af1b113c70716e81f87cd2e9a4d
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2023-06-28 18:21:59 +0600

    initial commit

---
+++ pacman.js
@@ -0,0 +1,84 @@
+class Pacman {
+    constructor(x, y, width, height, speed)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.direction = 4;
+        this.nextDirection = 4;
+        this.frameCount = 7;
+        this.currentFrame = 1;
+        setInterval(() => {
+            this.changeAnimation();
+        }, 100);
+    }
+
+    moveProcess()
+    {
+        this.changeDirectionIfPossible();
+        this.moveForwards();
+        if(this.checkCollisions())
+        {
+            this.moveBackwards();
+            return;
+        }
+    }
+
+    eat()
+    {
+        for(let i = 0; i < map.length; i++)
+        {
+            for(let j = 0; j < map[0].length; j++)
+            {
+                if(map[i][j] == 2 &&
+                   this.getMapX() == j &&
+                   this.getMapY() == i
+                )
+                {
+                    map[i][j] = 3;
+                    score++;
+                }
+            }
+        }
+    }
+
+    moveBackwards()
+    {
+        switch (this.direction) {
+            case DIRECTION_RIGHT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y += this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_BOTTOM:
+                this.y -= this.speed;
+                break;
+        }
+    }
+
+    moveForwards()
+    {
+        switch (this.direction)
+        {
+            case DIRECTION_RIGHT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y -= this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_BOTTOM:
+                this.y += this.speed;
+                break;
+        }
+    }
+
+}
